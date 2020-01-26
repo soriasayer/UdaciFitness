bf0a3b0ac2f4f6dd57583b07f32b3a0f031653bf
Soria <soriasayer@gmail.com>
2020-01-26 12:27:21 +0100

    Styleing added to the application

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Platform, StyleSheet } from 'react-native'
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utiles/helpers'
 import UdaceSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
@@ -9,12 +9,14 @@ import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utiles/api';
 import { connect } from "react-redux";
 import { addEntry } from '../actions'
+import { white, purple } from '../utiles/colors'
 
 function SubmitBtn({onPress}) {
     return(
         <TouchableOpacity
+        style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
         onPress={onPress}>
-            <Text>Submit</Text>
+            <Text style={styles.submitBtnText}>Submit</Text>
         </TouchableOpacity>
     )
 }
@@ -66,6 +68,7 @@ function SubmitBtn({onPress}) {
         this.props.dispatch(addEntry({
             [key]: entry
         }))
+        
         this.setState(() => ({
             run: 0,
             bike: 0,
@@ -85,7 +88,6 @@ function SubmitBtn({onPress}) {
         this.props.dispatch(addEntry({
             [key]: getDailyReminderValue()
         }))
-        console.log({[key]: getDailyReminderValue()})
         // Route to Home
 
         // Update 'DB'
@@ -97,26 +99,27 @@ function SubmitBtn({onPress}) {
         
         if(this.props.alreadyLogged) {
             return(
-                <View>
+                <View style={styles.center}>
                     <Ionicons 
-                        name='md-happy'
+                        name={Platform.OS === 'ios' ? 'ios-happy' : "md-happy"}
                         size={100}
                     />
                     <Text>You already logged your information for today</Text>
-                    <TextButton onPress={this.reset} >
+                    <TextButton style={{padding: 10}} onPress={this.reset} >
                         Reset
                     </TextButton>
                 </View>
             )
         }
         return(
-            <View>
+            <View style={styles.container}>
                 <DateHeader date={(new Date()).toLocaleDateString()} />
                {Object.keys(metaInfo).map((key) => {
                    const { getIcon, type, ...rest } = metaInfo[key]
                    const value = this.state[key]
+
                    return (
-                       <View key={key}>
+                       <View key={key} style={styles.row}>
                         {getIcon()}
                         {type === 'slider'
                             ? <UdaceSlider 
@@ -140,8 +143,54 @@ function SubmitBtn({onPress}) {
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        marginTop: 10,
+        backgroundColor: white,
+    },
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+    },
+    iosSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10,
+        borderRadius: 7,
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40,
+    },
+    androidSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 2,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center',
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 30,
+        marginLeft: 30,
+    }
+})
+
 function mapStateToProps (state) {
     const key = timeToString()
+
     return {
         alreadyLogged: state[key] && typeof state[key].today === 'undefined'
     }
